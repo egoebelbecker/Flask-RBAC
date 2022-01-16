32a5ea9ec28458f7a53fb348403bd7b3126cfdac
egoebelbecker <eric@ericgoebelbecker.com>
2022-01-16 14:42:11 -0500

    Initial working copy with primitive RBAC

--- app.py
+++ app.py
@@ -2,18 +2,76 @@
 # encoding: utf-8
 import json
 import sqlite3
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort,
 from werkzeug.security import check_password_hash
-from flask_httpauth import HTTPBasicAuth
+from flask_login import LoginManager, login_user, current_user, login_required
+import base64
+from functools import wraps
+from enum import IntEnum
 
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+app.secret_key = "ITJUSTDOESNTMATTER"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class Role(IntEnum):
+    MANAGER = 3
+    WAREHOUSE = 2
+    SALES = 1
+
+
+class User:
+    def __init__(self, name, role):
+        self.name = name
+        self.access_level = Role.SALES
+        if role == 'manager':
+            self.access_level = Role.MANAGER
+        elif role == 'warehouse':
+            self.access_level = Role.WAREHOUSE
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.name
+
+    def get_role(self):
+        return self.access_level
+
+    def allowed(self, access_level):
+        return self.access_level >= access_level
+
+
+def check_access(access_level):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.is_authenticated:
+                return abort(401)
+
+            if not current_user.allowed(access_level):
+                return abort(401)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
 
 
 # Retrieve an item
 @app.route('/api/inventory', methods=['GET'])
-@auth.login_required
+@login_required
+@check_access(Role.SALES)
 def query_records():
     try:
         name = request.args.get('name')
@@ -29,13 +87,13 @@ def query_records():
 
 # Modify an existing item
 @app.route('/api/inventory', methods=['PUT'])
-@auth.login_required
+@check_access(Role.WAREHOUSE)
 def update_record():
     try:
         record = json.loads(request.data)
         with sqlite3.connect("database.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO inventory (name, data) VALUES(?, ?) ON CONFLICT(name) DO UPDATE SET data=?",(record['name'], request.data, request.data))
+            cur.execute("INSERT INTO inventory (name, data) VALUES(?, ?) ON CONFLICT(name) DO UPDATE SET data=?", (record['name'], request.data, request.data))
             con.commit()
         return jsonify(record)
     except Exception as e:
@@ -44,7 +102,7 @@ def update_record():
 
 # Add a new item
 @app.route('/api/inventory', methods=['POST'])
-@auth.login_required
+@check_access(Role.MANAGER)
 def create_record():
     try:
         record = json.loads(request.data)
@@ -57,9 +115,10 @@ def create_record():
     except Exception as e:
         abort(500, e)
 
+
 # Delete an item
 @app.route('/api/inventory', methods=['DELETE'])
-@auth.login_required
+@check_access(Role.MANAGER)
 def delete_record():
     try:
         name = request.args.get('name')
@@ -73,18 +132,37 @@ def delete_record():
         abort(500, e)
 
 
-# Verify password against hashed values in the database
-@auth.verify_password
-def verify_password(username, password):
-    if username and password:
-        with sqlite3.connect("database.db") as con:
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute("select password from users where username=?", (username,))
-            row = cur.fetchone()
-            return check_password_hash(row['password'], password)
-    else:
-        return False
+@login_manager.request_loader
+def load_user_from_request(request_info):
+
+    # Try to login using Basic Auth
+    auth_header = request_info.headers.get('Authorization')
+    if auth_header:
+        auth_header = auth_header.replace('Basic ', '', 1)
+        try:
+            login_info = base64.b64decode(auth_header)
+            login_text = login_info.decode()
+            login_info = login_text.split(':')
+
+            user_info = retrieve_user(login_info[0])
+
+            if check_password_hash(user_info['password'], login_info[1]):
+                user = User(user_info['name'], user_info['role'])
+                login_user(user)
+                return user
+
+        except TypeError as e:
+            print(e)
+            return None
+
+
+def retrieve_user(username):
+    with sqlite3.connect("database.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("select * from users where name=?", (username,))
+        row = cur.fetchone()
+        return row
 
 
 if __name__ == '__main__':
